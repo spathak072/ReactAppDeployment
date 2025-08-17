c6a4cd9b6293a88e044e4356256a7d7b78ee17ad
spathak072 <saurabhpathak072@gmail.com>
2025-08-18 00:08:04 +0530

    updated page content

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Marketing from './Components/Marketing';
-
+import Header from './Components/Header';
+import {BrowserRouter} from 'react-router-dom';
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Hello World !!!!!!!!!!!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World Test 123
-        </a>
-      </header>
+      {/* <header className="App-header">
+       
+      </header> */}
+       <Header  />
       <Marketing/>
     </div>
+    </BrowserRouter>
   );
 }
 
